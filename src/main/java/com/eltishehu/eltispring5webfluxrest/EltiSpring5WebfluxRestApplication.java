0538eeddeba925fba35c1fd34a31224ab0fe3c17
Elti Shehu <shehuelti@gmail.com>
2018-10-19 17:08:29 +0200

    Initial project commit

---
+++ src/main/java/com/eltishehu/eltispring5webfluxrest/EltiSpring5WebfluxRestApplication.java
@@ -0,0 +1,12 @@
+package com.eltishehu.eltispring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EltiSpring5WebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EltiSpring5WebfluxRestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/eltishehu/eltispring5webfluxrest/EltiSpring5WebfluxRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eltishehu.eltispring5webfluxrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EltiSpring5WebfluxRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
